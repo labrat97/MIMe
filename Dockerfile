628b9d1ab1cc4a7a5109ca814b14ce31e236b0da
labrat97 <jamesmcroberts82997@gmail.com>
2021-08-17 03:08:28 -0700

    Keeping the horrors of TVM out of memory.

--- Dockerfile
+++ Dockerfile
@@ -3,15 +3,6 @@
 FROM nvcr.io/nvidia/l4t-ml:r32.6.1-py3 AS mime-base
 ENV PROGHOME=/mime
 WORKDIR ${PROGHOME}
-COPY . ${PROGHOME}/
-
-FROM nvcr.io/nvidia/deepstream-l4t:5.1-21.02-base AS tvm-builder
-RUN apt update && apt install -y build-essential cmake llvm-9-dev libclang-9-dev
-ENV BUILDHOME=/bld
-WORKDIR ${BUILDHOME}
-COPY . ${BUILDHOME}/
-RUN bash ${BUILDHOME}/perception/build-tvm.sh
 
 FROM mime-base AS mime-capture
-RUN rm -rf ${PROGHOME}/perception/tvm
-COPY --from=tvm-builder ${BUILDHOME}/perception/tvm ${PROGHOME}/perception/tvm
+COPY perception/. ${PROGHOME}/
---
+++ perception/captest.py
@@ -0,0 +1,58 @@
+import cv2 as cv
+import numpy as np
+import capture
+import vpi
+import gc
+WINDOW_NAME = 'Disparity'
+PVA_NVENC_VIC = vpi.Backend.PVA | vpi.Backend.NVENC | vpi.Backend.VIC
+
+captures = []
+retVal = True
+
+for idx in range(2):
+    config = capture.configurationString(idx, 1920, 1080, 30)
+    captures.append(cv.VideoCapture(config, cv.CAP_GSTREAMER))
+
+cv.startWindowThread()
+cv.namedWindow(WINDOW_NAME)
+cacheClr: int = 0
+
+while retVal:
+    if cacheClr % 5 == 0:
+        gc.collect()
+        vpi.clear_cache()
+    cacheClr = cacheClr + 1
+
+    frames = []
+    for cap in captures:
+        ret, frame = cap.read()
+        retVal = (retVal and ret)
+        if not retVal:
+            break
+        with vpi.Backend.CUDA:
+            gray = vpi.asimage(frame).convert(vpi.Format.Y16_ER).rescale((1920,1080))
+        with vpi.Backend.VIC:
+            grayBL = gray.convert(vpi.Format.Y16_ER_BL)
+        frames.append(grayBL)
+    
+    frames = frames[::-1]
+    confMap = vpi.Image(frames[0].size, vpi.Format.U16)
+    disp = vpi.stereodisp(frames[0], frames[1], out_confmap=confMap, backend=PVA_NVENC_VIC, window=5, maxdisp=256)
+    dispConv = disp.convert(vpi.Format.U8, backend=vpi.Backend.CUDA, scale=255.0/(32*256))
+    dispColor = cv.applyColorMap(dispConv.cpu(), cv.COLORMAP_JET)
+    
+    confMapConv = confMap.convert(vpi.Format.U8, backend=vpi.Backend.CUDA, scale=255.0/65535)
+    mask = cv.threshold(confMapConv.cpu(), 1, 255, cv.THRESH_BINARY)[1]
+    mask = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
+    dispColor = cv.bitwise_and(dispColor, mask)
+
+
+    if retVal:
+        cv.imshow(WINDOW_NAME, dispColor)
+    if cv.waitKey(10) & 0xFF == ord('q'):
+        cv.destroyWindow(WINDOW_NAME)
+        break
+
+for cap in captures:
+    cap.release()
+
