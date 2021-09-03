8f8f5caa4e5845b6340dca3cc14eee95f07ee8b4
labrat97 <jamesmcroberts82997@gmail.com>
2021-09-02 20:14:51 -0700

    It runs, that's all that's needed

--- perception/fakedepth/interop.py
+++ perception/fakedepth/interop.py
@@ -1,23 +1,33 @@
 import torch
-import numpy as np
 
-def loadModel(inputSize:torch.Size) -> tuple([torch.nn.Module, torch.nn.Module]):
+def loadModel() -> tuple([torch.nn.Module, torch.nn.Module]):
     INTEL_MIDAS = "intel-isl/MiDaS"
-    MIDAS_MODEL = "MiDaS"
+    MIDAS_MODEL = "MiDaS_small"
 
-    model = torch.hub.load(INTEL_MIDAS, MIDAS_MODEL).eval().cuda()
+    # Download from torchhub
+    model = torch.hub.load(INTEL_MIDAS, MIDAS_MODEL).cuda().eval()
     transform = torch.hub.load(INTEL_MIDAS, "transforms").small_transform
 
     return model, transform
 
-def pred(model:torch.nn.Module, transform:torch.nn.Module, image:np.array) -> torch.Tensor:
+def pred(model:torch.nn.Module, transform:torch.nn.Module, image, resize:bool=True) -> torch.Tensor:
+    # Convert to numpy array
+    if isinstance(image, torch.Tensor):
+        image = image.cpu().squeeze()
+        if len(image.shape) > 3:
+            result = [pred(model, transform, image[idx]).unsqueeze(0) for idx in range(image.size(0))]
+            return torch.cat(result)
+        image = image.numpy()
+
+    # Predict
     with torch.no_grad():
-        embeddedImage = transform(image).cuda()
-        rawDepth = model(embeddedImage).cuda()
+        embeddedImage = transform(image).cuda().detach()
+        rawDepth = model(embeddedImage).cuda().detach()
+        if not resize: return rawDepth.squeeze()
         depth = torch.nn.functional.interpolate(
             rawDepth.unsqueeze(1),
             size=image.shape[:2],
-            mode="bicubic",
+            mode="bilinear",
             align_corners=False,
         ).squeeze()
     
--- perception/init.sh
+++ perception/init.sh
@@ -2,11 +2,3 @@
 
 # Enables torch to handle pybind and the NVVPI functionality
 export TORCH_USE_RTLD_GLOBAL=YES
-
-# Downloads torch2trt in order to get the intel midas model running at a decent rate
-wget https://github.com/NVIDIA-AI-IOT/torch2trt/archive/refs/tags/v0.3.0.tar.gz
-tar -xzf v0.3.0.tar.gz
-cd torch2trt-0.3.0
-python3 setup.py install
-cd ..
-rm v0.3.0.tar.gz
