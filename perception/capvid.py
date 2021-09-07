f8fb1af09f984d2a0504f3bdf43fe3bae2a241f5
labrat97 <jamesmcroberts82997@gmail.com>
2021-09-07 13:42:01 -0700

    Added a way to capture and save video for later

---
+++ perception/capvid.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import numpy as np
+import syscamera
+import sys
+
+CAMERA_IDX:int = 0
+if len(sys.argv) > 1:
+    CAMERA_IDX = int(sys.argv[1])
+    print(f'Selected camera index {CAMERA_IDX} for capture...')
+
+CAMERA_SECOND:int = 21
+CAPTURE_TIME:int = 10*CAMERA_SECOND
+VID_FNAME:str = f'cam{CAMERA_IDX}cap.avi'
+
+capture = syscamera.config.setupCamera(CAMERA_IDX)
+width:int = int(capture.get(3))
+height:int = int(capture.get(4))
+size = (width, height)
+writer = cv.VideoWriter(VID_FNAME, cv.VideoWriter_fourcc(*'MJPG'), 10, frameSize=size)
+frames = np.zeros([CAPTURE_TIME, height, width, 3], dtype=np.uint8)
+framenum = 0
+
+while framenum < CAPTURE_TIME:
+    ret, frame = capture.read()
+    if not ret: break
+
+    frames[framenum] = frame
+    if framenum % int(CAMERA_SECOND/2) == 0:
+        print(float(framenum)/CAMERA_SECOND)
+
+    framenum += 1
+capture.release()
+
+for idx in range(frames.shape[0]):
+    writer.write(frames[idx])
+writer.release()
+
+print(f'The video was successfully saved to fname: \"{VID_FNAME}\"')
