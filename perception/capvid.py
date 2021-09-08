c9c8c706cae5146cd583189a8ab74e84b916a514
labrat97 <jamesmcroberts82997@gmail.com>
2021-09-08 11:17:03 -0700

    Did a little change for some remote testing

--- perception/capvid.py
+++ perception/capvid.py
@@ -9,20 +9,21 @@ if len(sys.argv) > 1:
     print(f'Selected camera index {CAMERA_IDX} for capture...')
 
 CAMERA_SECOND:int = 21
-CAPTURE_TIME:int = 10*CAMERA_SECOND
+CAPTURE_TIME:int = CAMERA_SECOND*CAMERA_SECOND
 VID_FNAME:str = f'cam{CAMERA_IDX}cap.avi'
 
 capture = syscamera.config.setupCamera(CAMERA_IDX)
-width:int = int(capture.get(3))
-height:int = int(capture.get(4))
+width:int = int(capture.get(3)/2)
+height:int = int(capture.get(4)/2)
 size = (width, height)
-writer = cv.VideoWriter(VID_FNAME, cv.VideoWriter_fourcc(*'MJPG'), 10, frameSize=size)
+writer = cv.VideoWriter(VID_FNAME, cv.VideoWriter_fourcc(*'MJPG'), 21, frameSize=size)
 frames = np.zeros([CAPTURE_TIME, height, width, 3], dtype=np.uint8)
 framenum = 0
 
 while framenum < CAPTURE_TIME:
     ret, frame = capture.read()
     if not ret: break
+    frame = cv.resize(frame, size, interpolation=cv.INTER_LINEAR)
 
     frames[framenum] = frame
     if framenum % int(CAMERA_SECOND/2) == 0:
